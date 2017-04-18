76b929b6e0b0e7c1682d902205af03bd9d9baca8
Jochem Berends <jochem.berends@ke-works.com>
2017-04-18 21:46:25 +0200

    sec4 les47 added activeBook reducer

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,10 @@
+// state argument is not application state, only the stat this reducer is responsible for
+// meaning how you actually wire the app up
+export default function (state = null, action) {
+    switch (action.type) {
+        case 'BOOK_SELECTED':
+            return action.payload;
+    }
+
+    return state;
+}
