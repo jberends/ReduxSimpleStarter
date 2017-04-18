384ea5e70ddac0b872bda702097b8318c7191f46
Jochem Berends <jochem.berends@ke-works.com>
2017-04-18 23:01:29 +0200

    sec5 les57 added actioncreator fetchweather with axios npm module

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,17 @@
+import axios from 'axios';
+
 const OPENWEATHERMAP_API_KEY = 'd6b1788a6f6e10b9fc0eae45eb3c78c5';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${OPENWEATHERMAP_API_KEY}`;
+const COUNTRY_CODE = 'nl';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},${COUNTRY_CODE}`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER
+    }
+}
 
