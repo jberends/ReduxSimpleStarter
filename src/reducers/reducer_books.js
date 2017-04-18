b10d3be620672b36438e53951a563a76e0029d3d
Jochem Berends <jochem.berends@ke-works.com>
2017-04-18 21:14:50 +0200

    sec3 lesson42 components and connect with mapStateToBookList

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'JavaScript: The Good Parts'},
+        { title: 'Harry Part'},
+        { title: 'The Dark Tower'},
+        { title: 'Dive into Python'}
+    ]
+}
